48f8d5fff940d476787a62cbcdc537804a971f7d
karahasanamer <akarahasan4@etf.unsa.ba>
2019-04-15 10:45:49 +0200

    master commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,13 +8,28 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        KorisniciModel model = new KorisniciModel();
+        model.napuni();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        loader.setController(new Controller(model));
+        Parent root = loader.load();
+
+        primaryStage.setTitle("Korisnici");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
+
+//    @Override
+//    public void start(Stage primaryStage) throws Exception{
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        primaryStage.setTitle("Hello World");
+//        primaryStage.setScene(new Scene(root, 300, 275));
+//        primaryStage.show();
+//    }
 
 
     public static void main(String[] args) {
