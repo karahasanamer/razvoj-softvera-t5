0e386be6f57cf811d985b1a9ebfbee55def0d9ad
karahasanamer <akarahasan4@etf.unsa.ba>
2019-04-15 10:43:00 +0200

    Klasa Controler

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,13 @@
 package sample;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Controller {
     public ListView LIspis;
@@ -11,10 +16,82 @@ public class Controller {
     public TextField tfMail;
     public TextField tfKorsIme;
     public TextField tfLozinka;
-
-    public void dodaj(ActionEvent actionEvent) {
-    }
-
+    private KorisniciModel model;
+    public ListView<Korisnik> lista;
+    @FXML
     public void kraj(ActionEvent actionEvent) {
+        Node n = (Node) actionEvent.getSource();
+        Stage stage = (Stage) n.getScene().getWindow();
+        stage.close();
     }
+    @FXML
+    public void dodaj(ActionEvent actionEvent) {
+
+        model.dodajKorisnika();
+        setTextPropetryUnBind();
+        lista.refresh();
+        lista.requestFocus();
+        lista.getSelectionModel().selectLast();
+    }
+
+    public Controller(KorisniciModel m) {
+        model = m;
+    }
+
+    @FXML
+    public void initialize(){
+        lista.requestFocus();
+        model.setTrenutniKorisnik(model.getKorisnik().get(0));
+        setTextPropetryBind();
+        lista.setItems(model.getKorisnik());
+        lista.getFocusModel().focus(0);
+        setTextPropetryUnBind();
+
+        lista.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Korisnik>() {
+            @Override
+            public void changed(ObservableValue<? extends Korisnik> observableValue, Korisnik korisnikOld, Korisnik korisnikNew) {
+                if (korisnikOld != null) {
+                    setTextPropetryUnBind();
+                }
+                if (korisnikNew == null) {
+                    tfIme.setText("");
+                    tfPrezime.setText("");
+                    tfMail.setText("");
+                    tfKorsIme.setText("");
+                    tfLozinka.setText("");
+                }
+                else {
+                    updateKorisnika();
+                }
+                lista.refresh();
+            }
+
+        });
+    }
+
+    private void updateKorisnika() {
+        Korisnik korisnik = (Korisnik) lista.getSelectionModel().getSelectedItem();
+        setTextPropetryUnBind();
+        model.setTrenutniKorisnik(korisnik);
+        setTextPropetryBind();
+        lista.refresh();
+    }
+
+    private void setTextPropetryBind() {
+        tfIme.textProperty().bindBidirectional(model.getTrenutniKorisnik().imeProperty());
+        tfPrezime.textProperty().bindBidirectional(model.getTrenutniKorisnik().prezimeProperty());
+        tfMail.textProperty().bindBidirectional(model.getTrenutniKorisnik().mailProperty());
+        tfKorsIme.textProperty().bindBidirectional(model.getTrenutniKorisnik().korisnickoImeProperty());
+        tfLozinka.textProperty().bindBidirectional(model.getTrenutniKorisnik().lozinkaProperty());
+    }
+
+    private void setTextPropetryUnBind() {
+        tfIme.textProperty().unbindBidirectional(model.getTrenutniKorisnik().imeProperty());
+        tfPrezime.textProperty().unbindBidirectional(model.getTrenutniKorisnik().prezimeProperty());
+        tfMail.textProperty().unbindBidirectional(model.getTrenutniKorisnik().mailProperty());
+        tfKorsIme.textProperty().unbindBidirectional(model.getTrenutniKorisnik().korisnickoImeProperty());
+        tfLozinka.textProperty().unbindBidirectional(model.getTrenutniKorisnik().lozinkaProperty());
+    }
+
+
 }
